e348bdce19fe4197c9c04ae46668f152a54f884a
tkaya <tkayasat@gmail.com>
2021-09-03 18:35:53 +0300

    4. Добавить кнопку в разметку и повесить на неё clickListener в Activity.

--- app/src/main/java/com/example/kotlincourse/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourse/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kotlincourse
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val field1 = "STATIC_STRING"
+    }
+
+    val listener = object : View.OnClickListener{
+        override fun onClick(p0: View?) {
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.Hello).setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
